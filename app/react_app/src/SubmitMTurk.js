c7fa1aa4d8f316e87765c64bab282b74c3c884bb
Josh Arnold <jarnold@ucdavis.edu>
2021-02-20 21:44:14 -0800

    test

--- app/react_app/src/SubmitMTurk.js
+++ app/react_app/src/SubmitMTurk.js
@@ -25,18 +25,34 @@ class SubmitMTurk extends React.Component {
     render() {
         if (this.state.pipeline == undefined) return <p>Loading...</p>;        
         return (
-            <form name="mturk_form" method="post" id="mturk_form" action="https://workersandbox.mturk.com/mturk/externalSubmit" onSubmit={this.formClicked}>
-              <input type="hidden" value={this.state.mturk.assignment_id} name="assignmentId" id={this.state.mturk.assignment_id}/>
-              <input type="hidden" value='foo' name="bar"/>
-              <input type="submit"/>
-            </form>
-        );     
-    }    
+            <div>
+                <form name="mturk_form" method="post" id="mturk_form" action="https://workersandbox.mturk.com/mturk/externalSubmit">
+                <input type="hidden" value={this.state.mturk.assignment_id} name="assignmentId" id={this.state.mturk.assignment_id}/>
+                <input type="hidden" value='foo' name="bar"/>
+                <input type="submit"/>
+                </form>
 
-    formClicked = (e) => {
-        e.preventDefault();
-        console.log("form submitted?");
-    }
+                <form name="mturk_form2" method="post" id="mturk_form2" action="workersandbox.mturk.com/mturk/externalSubmit">
+                <input type="hidden" value={this.state.mturk.assignment_id} name="assignmentId" id={this.state.mturk.assignment_id}/>
+                <input type="hidden" value='foo' name="bar"/>
+                <input type="submit"/>
+                </form>
+
+                <form name="mturk_form3" method="post" id="mturk_form3" action="/mturk/externalSubmit">
+                <input type="hidden" value={this.state.mturk.assignment_id} name="assignmentId" id={this.state.mturk.assignment_id}/>
+                <input type="hidden" value='foo' name="bar"/>
+                <input type="submit"/>
+                </form>
+
+                <form name="mturk_form4" method="post" id="mturk_form4" action="mturk/externalSubmit">
+                <input type="hidden" value={this.state.mturk.assignment_id} name="assignmentId" id={this.state.mturk.assignment_id}/>
+                <input type="hidden" value='foo' name="bar"/>
+                <input type="submit"/>
+                </form>                                
+
+            </div>
+        );     
+    }   
 
     notifyBackendToCompleteTask = () => {
         console.log("trying to notify backend");
