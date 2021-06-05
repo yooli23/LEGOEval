4a1f66b6c9435b26a800754dc0918108f5fee44a
Yoo Li <yuli23@language.cs.ucdavis.edu>
2021-06-05 06:51:31 -0700

    edit view data

--- app/view_data.py
+++ app/view_data.py
@@ -1,19 +1,24 @@
 from pprint import pprint
 from collections import Counter
+import argparse
 import json
 
 from util.data_reader import load_data
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task_name', required=True)
+    args = parser.parse_args()
 
-    task_name = "my-task-name"
-
-    print(f"Loading {task_name}\n")    
+    print(f"Loading {args.task_name}\n")    
     
-    data = load_data(task_name)
-        
-    for data_point in data:      
-        if data_point['complete']:
-            pass
-      
+    data = load_data(args.task_name)
+    
+    for data_point in data:
+        try:
+            if data_point['complete']:
+                print(data_point)
+            print('\n---\n')
+        except:
+            pass        
